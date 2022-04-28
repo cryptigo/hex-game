30b69c8453bace10aba90f7ff60dfe083432ee7b
Cryptigo <connorfrancey31@gmail.com>
2022-04-27 20:58:13 -0600

    first commit

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class HexGrid : MonoBehaviour {
+public class HexGrid : MonoBehaviour 
+{
 
 	public int width = 6;
 	public int height = 6;
@@ -11,40 +12,56 @@ public class HexGrid : MonoBehaviour {
 	public HexCell cellPrefab;
 	public Text cellLabelPrefab;
 
-	HexCell[] cells;
+	public Texture2D noiseSource;
 
-	Canvas gridCanvas;
-	HexMesh hexMesh;
+	// Private
 
-	void Awake () {
+	private HexCell[] cells;
+
+	private Canvas gridCanvas;
+	private HexMesh hexMesh;
+
+	private void OnEnable()
+	{
+		HexMetrics.noiseSource = noiseSource;
+	}
+
+	private void Awake () 
+	{
 		gridCanvas = GetComponentInChildren<Canvas>();
 		hexMesh = GetComponentInChildren<HexMesh>();
 
 		cells = new HexCell[height * width];
 
-		for (int z = 0, i = 0; z < height; z++) {
-			for (int x = 0; x < width; x++) {
+		for (int z = 0, i = 0; z < height; z++) 
+		{
+			for (int x = 0; x < width; x++) 
+			{
 				CreateCell(x, z, i++);
 			}
 		}
 	}
 
-	void Start () {
+	private void Start () 
+	{
 		hexMesh.Triangulate(cells);
 	}
 
-	public HexCell GetCell (Vector3 position) {
+	public HexCell GetCell (Vector3 position) 
+	{
 		position = transform.InverseTransformPoint(position);
 		HexCoordinates coordinates = HexCoordinates.FromPosition(position);
 		int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
 		return cells[index];
 	}
 
-	public void Refresh () {
+	public void Refresh () 
+	{
 		hexMesh.Triangulate(cells);
 	}
 
-	void CreateCell (int x, int z, int i) {
+	private void CreateCell (int x, int z, int i) 
+	{
 		Vector3 position;
 		position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
 		position.y = 0f;
@@ -56,19 +73,25 @@ public class HexGrid : MonoBehaviour {
 		cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
 		cell.color = defaultColor;
 
-		if (x > 0) {
+		if (x > 0) 
+		{
 			cell.SetNeighbor(HexDirection.W, cells[i - 1]);
 		}
-		if (z > 0) {
-			if ((z & 1) == 0) {
+		if (z > 0) 
+		{
+			if ((z & 1) == 0) 
+			{
 				cell.SetNeighbor(HexDirection.SE, cells[i - width]);
-				if (x > 0) {
+				if (x > 0) 
+				{
 					cell.SetNeighbor(HexDirection.SW, cells[i - width - 1]);
 				}
 			}
-			else {
+			else 
+			{
 				cell.SetNeighbor(HexDirection.SW, cells[i - width]);
-				if (x < width - 1) {
+				if (x < width - 1) 
+				{
 					cell.SetNeighbor(HexDirection.SE, cells[i - width + 1]);
 				}
 			}
@@ -76,8 +99,7 @@ public class HexGrid : MonoBehaviour {
 
 		Text label = Instantiate<Text>(cellLabelPrefab);
 		label.rectTransform.SetParent(gridCanvas.transform, false);
-		label.rectTransform.anchoredPosition =
-			new Vector2(position.x, position.z);
+		label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
 		label.text = cell.coordinates.ToStringOnSeparateLines();
 		cell.uiRect = label.rectTransform;
 	}
